14a9238eae3bcf4e8f43dd0b6df16a2e6a5a3caf
jeanvictorb <147008015+jeanvictorb@users.noreply.github.com>
2025-03-26 20:38:51 -0300

    asdfsdafafa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,12 @@ import path from 'path';
 import { Component, NgModule } from '@angular/core';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { LoginComponent } from './components/layout/login/login.component';
-import { ProfessorListComponent } from './components/professor/professor-list/professor-list.component';
-import { ProfessorFormComponent } from './components/professor/professor-form/professor-form.component';
-import { AlunoListComponent } from './components/aluno/aluno-list/aluno-list.component';
-import { AlunoFormComponent } from './components/aluno/aluno-form/aluno-form.component';
-import { CursoListComponent } from './components/curso/curso-list/curso-list.component';
-import { CursoFormComponent } from './components/curso/curso-form/curso-form.component';
-import { TurmaListComponent } from './components/turma/turma-list/turma-list.component';
-import { TurmaFormComponent } from './components/turma/turma-form/turma-form.component';
+import { PacoteFromComponent } from './components/pacotes/pacote-from/pacote-from.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: "login", component: LoginComponent},
-    {path: "admin", component: PrincipalComponent, children:[
-        {path: "professor", component: ProfessorListComponent},
-        {path: "professor/new", component: ProfessorFormComponent},
-        {path: "professor/edit/:id", component: ProfessorFormComponent},
-        {path: "aluno", component: AlunoListComponent},
-        {path: "aluno/new", component: AlunoFormComponent},
-        {path: "aluno/edit/:id", component: AlunoFormComponent},
-        {path: "curso", component: CursoListComponent},
-        {path: "turma", component: TurmaListComponent},
-        {path: "turma/new", component: TurmaFormComponent}      
-        
+    {path: '', redirectTo: 'principal', pathMatch: 'full'},
+    {path: "principal", component: PrincipalComponent, children: [
+        {path: 'pacote', component: PacoteFromComponent},
     ]},
 
 ];
---
+++ src/app/components/passeio/passeio-from/passeio-from.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passeio-from',
+  standalone: true,
+  imports: [],
+  templateUrl: './passeio-from.component.html',
+  styleUrl: './passeio-from.component.scss'
+})
+export class PasseioFromComponent {
+
+}
