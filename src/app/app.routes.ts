f633b6c281898274eb795e0225186659b100eceb
jeanvictorb <147008015+jeanvictorb@users.noreply.github.com>
2025-04-01 21:40:29 -0300

    card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Component, NgModule } from '@angular/core';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PacoteFromComponent } from './components/pacotes/pacote-from/pacote-from.component';
+import { PasseioFromComponent } from './components/passeio/passeio-from/passeio-from.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'principal', pathMatch: 'full'},
     {path: "principal", component: PrincipalComponent, children: [
         {path: 'pacote', component: PacoteFromComponent},
+        {path: 'passeio', component: PasseioFromComponent}
     ]},
 
 ];
--- src/app/components/passeio/passeio-from/passeio-from.component.ts
+++ src/app/components/passeio/passeio-from/passeio-from.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../card/card.component";
 
 @Component({
   selector: 'app-passeio-from',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './passeio-from.component.html',
   styleUrl: './passeio-from.component.scss'
 })
