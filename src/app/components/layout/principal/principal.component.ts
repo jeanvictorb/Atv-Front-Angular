c6681daa38049a8a2fd9dfd24a98251ee2e99827
NathaliaDN <nathziin@gmail.com>
2025-04-07 00:04:10 -0300

    Add more

--- src/app/components/layout/principal/principal.component.ts
+++ src/app/components/layout/principal/principal.component.ts
@@ -3,6 +3,7 @@ import { MenuComponent } from '../menu/menu.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../footer/footer.component";
 import { CardComponent } from "../../card/card.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-principal',
