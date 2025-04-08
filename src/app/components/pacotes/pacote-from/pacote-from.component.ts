1940f25b2740694d0bb574cf99433458e8125643
jeanvictorb <147008015+jeanvictorb@users.noreply.github.com>
2025-04-07 21:14:37 -0300

    adsf

--- src/app/components/pacotes/pacote-from/pacote-from.component.ts
+++ src/app/components/pacotes/pacote-from/pacote-from.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TourService } from '../../../service/tour.service';
+import { Tour } from '../../../models/Tour';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pacote-from',
@@ -9,5 +12,21 @@ import { CommonModule } from '@angular/common';
   styleUrl: './pacote-from.component.scss',
 })
 export class PacoteFromComponent {
+ lista: Tour[] = [];
  
+   tourService = inject(TourService);
+ 
+   constructor() {
+     this.findAll();
+   }
+ 
+   findAll() {
+     this.tourService.findAll()
+       .then((listaRetornada) => {
+         this.lista = listaRetornada;
+       })
+       .catch((erro) => {
+         Swal.fire(erro.message || 'Erro desconhecido', '', 'error');
+       });
+   }
 }
