98b47c3c14e0ff2bffe42745a9ebed34226947a2
jean <jeanvictor20032@gmail.com>
2025-04-08 18:32:23 -0300

    dfafs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './components/layout/login/login.component';
 import { PacoteFromComponent } from './components/pacotes/pacote-from/pacote-from.component';
 import { PasseioFromComponent } from './components/passeio/passeio-from/passeio-from.component';
 import { CreaterComponent } from './components/layout/creater/creater.component';
+import { Login } from './models/login';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'principal', pathMatch: 'full'},
     {path: "principal", component: PrincipalComponent, children: [
         {path: 'pacotes', component: PacoteFromComponent},
         {path: 'passeio', component: PasseioFromComponent},
-        {path: 'creater', component: CreaterComponent}
+        {path: 'creater', component: CreaterComponent},
+        {path: 'login', component: LoginComponent}
 
     ]},
 
