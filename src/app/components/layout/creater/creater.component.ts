a84b25ad9e6a0798ed7e8901d84abe56d8563cff
jean <jeanvictor20032@gmail.com>
2025-04-08 17:44:06 -0300

    creater

--- src/app/components/layout/creater/creater.component.ts
+++ src/app/components/layout/creater/creater.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-creater',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './creater.component.html',
   styleUrl: './creater.component.scss'
 })
@@ -11,4 +13,17 @@ export class CreaterComponent {
   nameCard!: string;
   descriptionCard!: string;
 
+  cadastroRealizado = false;
+
+  cadastrarPasseio() {
+    console.log('Passeio:', this.nameCard, this.descriptionCard);
+  
+    this.cadastroRealizado = true;
+  
+    // Esconde o alerta após 3 segundos (opcional)
+    setTimeout(() => {
+      this.cadastroRealizado = false;
+    }, 3000);
+  }
+  
 }
