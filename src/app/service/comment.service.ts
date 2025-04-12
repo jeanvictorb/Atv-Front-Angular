47ed464b1311c1f9df94bb4ba07c74f68f484281
jean <jeanvictor20032@gmail.com>
2025-04-12 18:23:52 -0300

    comment

---
+++ src/app/service/comment.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { error } from "console";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CommentService {
+
+    private API = 'http://localhost:8080/comment'
+
+    constructor() {}
+    
+    findAll(): Promise<Comment[]>{
+        return fetch(this.API)
+        .then((response) =>{
+            if(!response.ok) {
+                throw new Error('Erro na requisição');
+            }
+            return response.json();
+        })
+        .then((data) => data as Comment[])
+        .catch((error) => {
+            throw  new Error(error);
+        })
+    }
+
+    
+}
